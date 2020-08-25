eb34d2e36fa42dd48861864b5ab131de9919a591
Daniel A <dannyboy3385@gmail.com>
2020-08-25 14:04:02 -0700

    orm built

---
+++ config/orm.js
@@ -0,0 +1,54 @@
+const db = require('./connection.js')
+const { getDefaultFlags } = require('mysql2/lib/connection_config')
+
+// db.query('SELECT * FROM groceries', (err, groceries) => {
+//   if (err) {console.log(err)}
+// })
+
+const orm = {}
+
+orm.create = (table, data, cb) => {
+  db.query(`INSERT INTO ${table} SET ?`, data, (err, fields) => {
+    if (err){console.log(err)}
+    cb(fields.insertId)
+  })
+}
+
+// orm.create('groceries', {name: 'Pork Patties', quantity: 15, cost: 10.00}, id => {
+//   console.log(id)
+// })
+
+orm.read = (table, cb) => {
+  db.query(`SELECT * FROM ${table}`, (err, data) => {
+    if (err) {console.log(err)}
+    cb(data)
+  })
+}
+
+// orm.read('groceries', groceries => {
+//   console.log(groceries)
+// })
+
+orm.update = (table, updates, where, cb) => {
+  db.query(`UPDATE ${table} SET ? WHERE ?`, [updates, where], err => {
+    if (err){console.log(err)}
+    cb()
+  })
+}
+
+// orm.update('groceries', {name: 'Doritos'}, {id: 8}, () => {
+//   console.log('Updated!')
+// })
+
+orm.delete = (table, where,cb) => {
+  db.query(`DELETE FROM ${table} WHERE ?`, where, err => {
+    if (err) {console.log(err)}
+    cb()
+  })
+}
+
+// orm.delete('groceries', {id: 11}, () => {
+//   console.log('Deleted!')
+// })
+
+module.exports = orm
---
+++ sql/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO groceries(name, quantity, cost)
+VALUES('Apple', 25, 2.50), 
+('Pineapple', 30, 5.00), 
+('Milk', 150, 4.60), 
+('Swiss Cheese', 250, 3.00), 
+('Eggs', 58, 8.00), 
+('Banana Bundle', 50, 4.00), 
+('Coca-Cola', 500, 2.00), 
+('Nacho Cheese Doritos', 55, 3.00), 
+('Dasani Water', 300, 1.00), 
+('Pear', 400, 1.50);
