338710013545fe649ed9d818a8555add22c5dc4c
Daniel A <dannyboy3385@gmail.com>
2020-08-28 10:13:25 -0700

    database set

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,11 +1,12 @@
+USE hq26jwuz7qk7i21t;
 INSERT INTO burgers(name, quantity, cost, purchased)
-VALUES('Apple', 25, 2.50, false), 
-('Pineapple', 30, 5.00, false), 
-('Milk', 150, 4.60, false), 
-('Swiss Cheese', 250, 3.00, false), 
-('Eggs', 58, 8.00, false), 
-('Banana Bundle', 50, 4.00, false), 
-('Coca-Cola', 500, 2.00, false), 
-('Nacho Cheese Doritos', 55, 3.00, false), 
-('Dasani Water', 300, 1.00, false), 
-('Pear', 400, 1.50, false);
+VALUES('Sloppy Joe', 25, 2.50, false), 
+('Hawaii', 30, 5.00, false), 
+('Pork', 150, 4.60, false), 
+('Chicken', 250, 3.00, false), 
+('Egg', 58, 8.00, false), 
+('Banana', 50, 4.00, false), 
+('Vegan', 500, 2.00, false), 
+('Beyond Meat', 55, 3.00, false), 
+('Cheese', 300, 1.00, false), 
+('Blue Cheese', 400, 1.50, false);
